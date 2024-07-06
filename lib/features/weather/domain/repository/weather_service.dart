2773cd864961e859b7279e5717ef8f0ed5529fdb
JayeshPatil18 <jp7470484@gmail.com>
2024-07-07 02:56:56 +0530

    done-final

--- lib/features/weather/domain/repository/weather_service.dart
+++ lib/features/weather/domain/repository/weather_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  static const String _apiKey = 'bd5e378503939ddaee76f12ad7a97608';
+  static const String _apiKey = 'bd5e378503939ddaee76f12ad7a97608'; // <YOUR_API_KEY>
   static const String _baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
   Future<Map<String, dynamic>> fetchWeather(String city) async {
