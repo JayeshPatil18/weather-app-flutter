8f492e1f4b3ce3251607057473785519235d90cd
JayeshPatil18 <jp7470484@gmail.com>
2024-07-07 00:59:21 +0530

    safe

---
+++ lib/common/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+// Define all the colors used in the UI
+final Color primaryColor = Color(0xFF000000);
+final Color secondaryColor = Color(0xFF1A1A1A);
+final Color highlightColor = Colors.blueAccent;
+final Color lightText = Colors.grey;
+
+MaterialColor mainAppColor = const MaterialColor(0xFF000000, <int, Color>{
+  50: Color(0xFF000000),
+  100: Color(0xFF000000),
+  200: Color(0xFF000000),
+  300: Color(0xFF000000),
+  400: Color(0xFF000000),
+  500: Color(0xFF000000),
+  600: Color(0xFF000000),
+  700: Color(0xFF000000),
+  800: Color(0xFF000000),
+  900: Color(0xFF000000),
+});
--- lib/features/weather/presentation/screens/home_screen.dart
+++ lib/features/weather/presentation/screens/home_screen.dart
@@ -1,26 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:weather_app/common/colors.dart';
 import 'package:weather_app/features/weather/presentation/screens/weather_screen.dart';
 
 import '../provider/weather_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  static String searchText = '';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _cityController = TextEditingController();
+
+  final FocusNode _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Weather App')),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Weather App', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+        backgroundColor: secondaryColor,
+        elevation: 0,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextField(
-              controller: _cityController,
-              decoration: InputDecoration(
-                labelText: 'Enter city name',
-                border: OutlineInputBorder(),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: TextField(
+                          style: TextStyle(color: Colors.white),
+                          focusNode: _focusNode,
+                          controller: _cityController,
+                          // cursorHeight: cursorHeight,
+                          cursorColor: highlightColor,
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.only(
+                                top: 16, bottom: 16, left: 20, right: 45),
+                            fillColor: secondaryColor,
+                            filled: true,
+                            hintStyle: TextStyle(color: lightText),
+                            hintText: 'Enter City Name',
+                            border: OutlineInputBorder(
+                              borderRadius:
+                              BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                width: 0,
+                                style: BorderStyle.none,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                BorderRadius.circular(30),
+                                borderSide: BorderSide(
+                                    width: 1.0, color: highlightColor)),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                          right: 8,
+                          child: _cityController.text != ''
+                              ? GestureDetector(
+                            onTap: () {
+                              _cityController.text = '';
+                              setState(() {
+                                HomeScreen.searchText = '';
+                              });
+                            },
+                            child: Container(
+                              padding: EdgeInsets.only(right: 4),
+                              child: Icon(Icons.close,
+                                  color: lightText),
+                            ),
+                          )
+                              : Container(
+                            padding: EdgeInsets.only(right: 4),
+                            child: Icon(Icons.search,
+                                color: _focusNode.hasFocus
+                                    ? highlightColor
+                                    : lightText),
+                          )),
+
+                      // Positioned(
+                      //     right: 8,
+                      //     child: CircleIconContainer(
+                      //         icon: const Icon(Icons.search,
+                      //             color: AppColors.primaryColor30),
+                      //         containerColor: AppColors.secondaryColor10,
+                      //         containerSize: 40)),
+                    ],
+                  ),
+                  SizedBox(height: 20)
+                ],
               ),
             ),
             SizedBox(height: 20),
@@ -35,7 +119,15 @@ class HomeScreen extends StatelessWidget {
                   );
                 }
               },
-              child: Text('Get Weather'),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                child: Text('Get Weather', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: highlightColor,
+                padding: EdgeInsets.symmetric(vertical: 16),
+                textStyle: TextStyle(fontSize: 18),
+              ),
             ),
           ],
         ),
